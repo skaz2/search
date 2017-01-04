78879d8a1111f21ceda8cee1dbbc3b936db5712d
skaz <admin@Mac-Admin.local>
2017-01-04 10:54:06 +0300

    start

---
+++ search/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  search
+//
+//  Created by Admin on 04.01.17.
+//  Copyright © 2017 Sergey Kharlamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var geoFire: GeoFire!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
---
+++ search/search-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  search-Bridging-Header.h
+//  search
+//
+//  Created by Admin on 04.01.17.
+//  Copyright © 2017 Sergey Kharlamov. All rights reserved.
+//
+
+#import "GeoFire.h"
